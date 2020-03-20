cc81a649278bf37b870df7ed78b36fa0f46daeb6
araneoAnicet <araneoanicet@gmail.com>
2020-03-21 01:57:07 +0200

    remove expression index

--- Token.h
+++ Token.h
@@ -4,15 +4,13 @@
 
 
 struct Token {
-    Token(std::string name, std::string type, TokenValue* value, int line_index, int expression_index) {
+    Token(std::string name, std::string type, TokenValue* value, int line_index) {
         this->name = name;
         this->type = type;
         this->value = value;
         this->line_index = line_index;
-        this->expression_index = expression_index;
     }
     int line_index;
-    int expression_index;
     TokenValue* value;
     std::string type;
     std::string name;
