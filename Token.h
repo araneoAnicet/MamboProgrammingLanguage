af58fedb086b695aa1ea1fb2cd1dd5c749dc7993
araneoAnicet <araneoanicet@gmail.com>
2020-03-21 17:54:47 +0200

    use type indexes for tokens instead of strings

--- Token.h
+++ Token.h
@@ -2,15 +2,23 @@
 #include "TokenValue.h"
 #include <string>
 
+// token types
+#define MMB_STR 0
+#define MMB_NUM 1
+#define MMB_KEY 2
+#define MMB_COMM 3
+#define MMB_VAR 4
+
+
 
 struct Token {
-    Token(std::string name, std::string type, TokenValue* value) {
+    Token(std::string name, int type, std::string value) {
         this->name = name;
         this->type = type;
         this->value = value;
     }
     int line_index;
-    TokenValue* value;
-    std::string type;
+    std::string value;
+    int type;
     std::string name;
 };
--- TokenValue.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-
-class TokenValue {
-public:
-    virtual std::string represent() = 0;
-    virtual double get() = 0;
-};
