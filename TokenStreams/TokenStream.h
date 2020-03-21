4c8473c6da37873a717a202c0d53a09a53936544
araneoAnicet <araneoanicet@gmail.com>
2020-03-21 18:36:29 +0200

    add instance generation method

--- TokenStreams/TokenStream.h
+++ TokenStreams/TokenStream.h
@@ -5,11 +5,12 @@
 
 class TokenStream {
 protected:
-    TokenStream* instance = nullptr;
+    static TokenStream* instance;
     bool token_is_generated;
     std::string string_input;
     Token* token_addr;
 public:
+    virtual void generate_instance() = 0;
     void set_string_input(std::string string_input);
     void set_token_addr(Token* token_addr);
     bool has_generated_token();
