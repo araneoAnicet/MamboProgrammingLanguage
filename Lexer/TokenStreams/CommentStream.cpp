e989a18c42143fd882da5091db57feba5d0ca51b
araneoAnicet <araneoanicet@gmail.com>
2020-05-02 16:31:26 +0100

    remove char shift

--- Lexer/TokenStreams/CommentStream.cpp
+++ Lexer/TokenStreams/CommentStream.cpp
@@ -5,6 +5,5 @@ void CommentStream::pattern_check(char input) {
         this->next_state = std::make_shared<KeywordStream>();
         this->next_state->set_prev_char(input);
         this->has_changed_state = true;
-        this->stream_switcher.shift_chars(input);
     }
 }
