34101fa5aedfe26e6df519309742ba53e6143560
araneoAnicet <araneoanicet@gmail.com>
2020-05-02 16:24:05 +0100

    set the previous char for the next state

--- Lexer/TokenStreams/CommentStream.cpp
+++ Lexer/TokenStreams/CommentStream.cpp
@@ -3,6 +3,7 @@
 void CommentStream::pattern_check(char input) {
     if (input == '\n') {
         this->next_state = std::make_shared<KeywordStream>();
+        this->next_state->set_prev_char(input);
         this->has_changed_state = true;
         this->stream_switcher.shift_chars(input);
     }
