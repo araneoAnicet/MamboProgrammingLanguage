cc83c2305f0b49671c305bddc9a33b896cda4f09
araneoAnicet <araneoanicet@gmail.com>
2020-05-02 16:38:58 +0100

    set token address for next state

--- Lexer/TokenStreams/CommentStream.cpp
+++ Lexer/TokenStreams/CommentStream.cpp
@@ -4,6 +4,7 @@ void CommentStream::pattern_check(char input) {
     if (input == '\n') {
         this->next_state = std::make_shared<KeywordStream>();
         this->next_state->set_prev_char(input);
+        this->next_state->set_token_addr(this->token_addr);
         this->has_changed_state = true;
     }
 }
