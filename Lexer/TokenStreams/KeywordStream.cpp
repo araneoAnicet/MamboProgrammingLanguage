db9c7680e87d09b2cc965603352951ac9d9df893
araneoAnicet <araneoanicet@gmail.com>
2020-05-11 23:15:43 +0100

    Use state switcher for implementing pattern checking

--- Lexer/TokenStreams/KeywordStream.cpp
+++ Lexer/TokenStreams/KeywordStream.cpp
@@ -20,48 +20,69 @@ void KeywordStream::set_prev_char(char prev_char) {
     this->tree.find_keyword(prev_char);
     if (this->tree.has_found_not_keyword()) {
         this->tree.refresh_temp_tree();
-        this->ignore_input = true;
+        // fix here
     }
 }
 
-void KeywordStream::pattern_check(char input) {
-    Token* temp_token_addr;
 
-    if (input == '\'') {
-        if (this->tree.has_found_keyword()) {
-            temp_token_addr = Tokenizer::generate_token(MMB_KEY, tree.get_keyword());
-            *(this->token_addr) = *temp_token_addr;
-            delete temp_token_addr;
-            this->token_is_generated = true;
+void KeywordStream::switch_state() {
+    this->next_state = this->stream_switcher.get_stream();
+    this->next_state->set_token_addr(this->token_addr);
+    this->state_is_changed = true;
+}
+
+void KeywordStream::record_token() {
+    Token* temp_token_addr;
+    if (this->tree.has_found_keyword()) {
+        temp_token_addr = Tokenizer::generate_token(MMB_KEY, this->tree.get_keyword());
+    } else {
+        if (this->lexemes_have_only_digits) {
+            temp_token_addr = Tokenizer::generate_token(MMB_NUM, this->string_input);
+        } else {
+            temp_token_addr = Tokenizer::generate_token(MMB_VAR, this->string_input);
         }
-        this->next_state = std::make_shared<StringStream>();
-        this->next_state->set_token_addr(this->token_addr);
-        this->state_is_changed = true;
+    }
+    *(this->token_addr) = *temp_token_addr;
+    delete temp_token_addr;
+    this->token_is_generated = true;
+}
+
+void KeywordStream::record_token_and_switch_state() {
+    this->record_token();
+    this->switch_state();
+}
+
+void KeywordStream::pattern_check(char input) {
+
+    this->stream_switcher.shift_chars(input);
+    this->tree.find_keyword(input);
+
+    if (!(this->stream_switcher.check_number())) {
+        this->lexemes_have_only_digits = false;
     }
 
-    if (
-        Tokenizer::dividing_symbols.find(input) != Tokenizer::dividing_symbols.end()
-        || input == ' '
-        || input == '\n'
-        || input == '\t'
-    ) {
-
-        if (this->tree.has_found_keyword()) {
-            temp_token_addr = Tokenizer::generate_token(MMB_KEY, tree.get_keyword());
-            *(this->token_addr) = *temp_token_addr;
-            delete temp_token_addr;
-            this->token_is_generated = true;
-        }
-        this->tree.refresh_temp_tree();
-        this->ignore_input = false;
+    if (this->stream_switcher.check_ignored_symbol()) {  // next state is KeywordStream    
+        this->record_token_and_switch_state();
+        this->next_state->set_prev_char(input);
         return;
     }
-    if (!this->ignore_input) {
-        this->tree.find_keyword(input);
-        if (this->tree.has_found_not_keyword()) {
-            this->tree.refresh_temp_tree();
-            this->ignore_input = true;
-        }
+
+    if (this->stream_switcher.check_string()) {    
+        this->record_token_and_switch_state();
+        this->next_state->set_prev_char(input);
+        return;
     }
-    this->prev_char = input;
+
+    if (this->stream_switcher.check_dividing_symbol()) {
+            this->record_token_and_switch_state();
+            this->next_state->set_prev_char(input);
+            return;
+    }
+
+    if (this->stream_switcher.check_variable()) {
+        this->switch_state();
+        this->next_state->set_string_input(this->string_input);
+        this->next_state->set_prev_char(input);
+    }
+    
 }
--- Lexer/TokenStreams/KeywordStream.h
+++ Lexer/TokenStreams/KeywordStream.h
@@ -8,7 +8,10 @@
 class KeywordStream: public TokenStream {
 private:
     KeywordsTree tree;
-    bool ignore_input = false;
+    bool lexemes_have_only_digits = true;
+    void switch_state();
+    void record_token();
+    void record_token_and_switch_state();
 public:
     KeywordStream();
     void set_prev_char(char prev_char);
